5c218639be07910783b5f1239e8b8262f6f3b91a
koteld <festbot@gmail.com>
2023-04-03 21:01:05 +0800

    add/update Helper scripts

--- script/Helper.s.sol
+++ script/Helper.s.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../src/LinkToken.sol";
+import "chainlink/v0.8/interfaces/LinkTokenInterface.sol";
 
-contract Transfer is Script {
+contract HelperScript is Script {
   function run() external {
     console.log("Please run transferEth(uint256) or transferLink(uint256,address) method.");
   }
@@ -25,11 +25,9 @@ contract Transfer is Script {
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(deployerPrivateKey);
 
-    LinkToken linkToken = LinkToken(tokenAddress);
+    LinkTokenInterface linkToken = LinkTokenInterface(tokenAddress);
     linkToken.transfer(recipientAddress, amount);
 
     vm.stopBroadcast();
-
-
   }
 }

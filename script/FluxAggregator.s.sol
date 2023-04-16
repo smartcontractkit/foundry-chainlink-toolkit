974e227619d169ae57bbca31892428e1befef46e
koteld <festbot@gmail.com>
2023-04-16 12:02:06 +0800

    update Flux Aggregator Solidity Script

--- script/FluxAggregator.s.sol
+++ script/FluxAggregator.s.sol
@@ -10,14 +10,57 @@ contract FluxAggregatorScript is Script {
     console.log("Please run deploy() method.");
   }
 
-  function deploy(address tokenAddress) external {
+  function deploy(address tokenAddress) external returns(address) {
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-    uint256 deployerAddress = vm.envUint("DEPLOYER_ADDRESS");
     vm.startBroadcast(deployerPrivateKey);
 
     MockAggregatorValidator mockAggregatorValidator = new MockAggregatorValidator();
-    FluxAggregator fluxAggregator = new FluxAggregator(tokenAddress, 1, 30, address(mockAggregatorValidator), 0, 1000000000000, 0, "Test Flux Aggregator");
+    FluxAggregator fluxAggregator = new FluxAggregator(tokenAddress, 2000000, 30, address(mockAggregatorValidator), 0, 1000000000000, 0, "Test Flux Aggregator");
 
     vm.stopBroadcast();
+
+    return address(fluxAggregator);
+  }
+
+  function updateAvailableFunds(address fluxAggregatorAddress) external returns(uint128) {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+
+    FluxAggregator fluxAggregator = FluxAggregator(fluxAggregatorAddress);
+    fluxAggregator.updateAvailableFunds();
+
+    vm.stopBroadcast();
+
+    return fluxAggregator.availableFunds();
+  }
+
+  function setOracles(address fluxAggregatorAddress, address[] calldata nodesArray) external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+
+    address[] memory removed;
+
+    FluxAggregator fluxAggregator = FluxAggregator(fluxAggregatorAddress);
+    fluxAggregator.changeOracles(removed, nodesArray, nodesArray, 3, 3, 0);
+
+    vm.stopBroadcast();
+  }
+
+  function getOracles(address fluxAggregatorAddress) external returns(address[] memory) {
+    FluxAggregator fluxAggregator = FluxAggregator(fluxAggregatorAddress);
+    address[] memory oracles = fluxAggregator.getOracles();
+    return oracles;
+  }
+
+  function getLatestAnswer(address fluxAggregatorAddress) external returns (int256) {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+
+    FluxAggregator fluxAggregator = FluxAggregator(fluxAggregatorAddress);
+    int256 answer = fluxAggregator.latestAnswer();
+
+    vm.stopBroadcast();
+
+    return answer;
   }
 }

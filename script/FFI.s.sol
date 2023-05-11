9ac76b7f9c0062a6b66cd425b0b5624c8fc6be3f
koteld <festbot@gmail.com>
2023-05-11 19:17:03 +0200

    update FFI Solidity Script

--- script/FFI.s.sol
+++ script/FFI.s.sol
@@ -13,130 +13,16 @@ contract FFIScript is Script {
   function getNodeAddress(string memory nodeId) public returns(address) {
     string[] memory cmds = new string[](3);
     cmds[0] = "make";
-    cmds[1] = "get-node-address";
+    cmds[1] = "fct-get-node-address";
     cmds[2] = Utils.append("NODE_ID=", nodeId);
     bytes memory nodeAddress = vm.ffi(cmds);
     return Utils.bytesToAddress(nodeAddress);
   }
 
-  function getJobId(string memory nodeId, address contractAddress) public returns(string memory) {
-    string[] memory cmds = new string[](5);
-    string memory prefix = "0_";
-    cmds[0] = "make";
-    cmds[1] = "get-job-id";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("CONTRACT_ADDRESS=", vm.toString(contractAddress));
-    cmds[4] = Utils.append("PREFIX=", prefix);
-    bytes memory jobId = vm.ffi(cmds);
-    return Utils.removePrefix(prefix, string(jobId));
-  }
-
-  function getExternalJobId(string memory nodeId, address contractAddress) public returns(string memory) {
-    string[] memory cmds = new string[](4);
-    string memory prefix = "0x";
-    cmds[0] = "make";
-    cmds[1] = "get-external-job-id";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("CONTRACT_ADDRESS=", vm.toString(contractAddress));
-    bytes memory externalJobId = vm.ffi(cmds);
-    return Utils.removePrefix(prefix, vm.toString(externalJobId));
-  }
-
-  function getLastWebhookJobId(string memory nodeId) public returns(string memory) {
-    string[] memory cmds = new string[](4);
-    string memory prefix = "0_";
-    cmds[0] = "make";
-    cmds[1] = "get-last-webhook-job-id";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("PREFIX=", prefix);
-    bytes memory jobId = vm.ffi(cmds);
-    return Utils.removePrefix(prefix, string(jobId));
-  }
-
-  function createDirectRequestJob(string memory nodeId, address oracle) public {
-    string[] memory cmds = new string[](4);
-    cmds[0] = "make";
-    cmds[1] = "create-direct-request-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("ORACLE_ADDRESS=", vm.toString(oracle));
-    vm.ffi(cmds);
-  }
-
-  function createCronJob(string memory nodeId, address cronConsumer) public {
-    string[] memory cmds = new string[](4);
-    cmds[0] = "make";
-    cmds[1] = "create-cron-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("CRON_CONSUMER_ADDRESS=", vm.toString(cronConsumer));
-    vm.ffi(cmds);
-  }
-
-  function createWebhookJob(string memory nodeId) public {
-    string[] memory cmds = new string[](3);
-    cmds[0] = "make";
-    cmds[1] = "create-webhook-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    vm.ffi(cmds);
-  }
-
-  function runWebhookJob(string memory nodeId) public {
-    string[] memory cmds = new string[](3);
-    cmds[0] = "make";
-    cmds[1] = "run-webhook-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    vm.ffi(cmds);
-  }
-
-  function createKeeperJob(string memory nodeId, address registry) public {
-    string[] memory cmds = new string[](4);
-    cmds[0] = "make";
-    cmds[1] = "create-keeper-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("REGISTRY_ADDRESS=", vm.toString(registry));
-    vm.ffi(cmds);
-  }
-
-  function createFluxJob(string memory nodeId, address fluxAggregator) public {
-    string[] memory cmds = new string[](4);
-    cmds[0] = "make";
-    cmds[1] = "create-flux-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("FLUX_AGGREGATOR_ADDRESS=", vm.toString(fluxAggregator));
-    vm.ffi(cmds);
-  }
-
-  function createOcrBootstrapJob(string memory nodeId, address offchainAggregator) public {
-    string[] memory cmds = new string[](4);
-    cmds[0] = "make";
-    cmds[1] = "create-ocr-bootstrap-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("OFFCHAIN_AGGREGATOR_ADDRESS=", vm.toString(offchainAggregator));
-    vm.ffi(cmds);
-  }
-
-  function createOcrJob(string memory nodeId, address offchainAggregator, string memory bootstrapPeerId) public {
-    string[] memory cmds = new string[](5);
-    cmds[0] = "make";
-    cmds[1] = "create-ocr-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("OFFCHAIN_AGGREGATOR_ADDRESS=", vm.toString(offchainAggregator));
-    cmds[4] = Utils.append("BOOTSTRAP_P2P_KEY=", bootstrapPeerId);
-    vm.ffi(cmds);
-  }
-
-  function deleteJob(string memory nodeId, string memory jobId) public {
-    string[] memory cmds = new string[](4);
-    cmds[0] = "make";
-    cmds[1] = "delete-job";
-    cmds[2] = Utils.append("NODE_ID=", nodeId);
-    cmds[3] = Utils.append("JOB_ID=", jobId);
-    vm.ffi(cmds);
-  }
-
   function getNodeConfig(string memory nodeId) public returns(string[] memory) {
     string[] memory cmds = new string[](3);
     cmds[0] = "make";
-    cmds[1] = "get-node-config";
+    cmds[1] = "fct-get-node-config";
     cmds[2] = Utils.append("NODE_ID=", nodeId);
     bytes memory res = vm.ffi(cmds);
     // NOTE: workaround to split node configuration string
@@ -154,7 +40,7 @@ contract FFIScript is Script {
   ) public returns(string[] memory) {
     string[] memory cmds = new string[](7);
     cmds[0] = "make";
-    cmds[1] = "get-ocr-config";
+    cmds[1] = "fct-prepare-ocr-config";
     cmds[2] = Utils.append("NODE_ADDRESSES=", nodeAddressesStr);
     cmds[3] = Utils.append("OFFCHAIN_PUBLIC_KEYS=", offChainPublicKeysStr);
     cmds[4] = Utils.append("CONFIG_PUBLIC_KEYS=", configPublicKeysStr);
@@ -165,5 +51,120 @@ contract FFIScript is Script {
     vm.setEnv("OCR_CONFIG", string(res));
     string[] memory ocrConfig = vm.envString("OCR_CONFIG"," ");
     return ocrConfig;
+  }
+
+  function getJobId(string memory nodeId, address contractAddress) public returns(string memory) {
+    string[] memory cmds = new string[](5);
+    string memory prefix = "0_";
+    cmds[0] = "make";
+    cmds[1] = "fct-get-job-id";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("CONTRACT_ADDRESS=", vm.toString(contractAddress));
+    cmds[4] = Utils.append("PREFIX=", prefix);
+    bytes memory jobId = vm.ffi(cmds);
+    return Utils.removePrefix(prefix, string(jobId));
+  }
+
+  function getExternalJobId(string memory nodeId, address contractAddress) public returns(string memory) {
+    string[] memory cmds = new string[](4);
+    string memory prefix = "0x";
+    cmds[0] = "make";
+    cmds[1] = "fct-get-external-job-id";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("CONTRACT_ADDRESS=", vm.toString(contractAddress));
+    bytes memory externalJobId = vm.ffi(cmds);
+    return Utils.removePrefix(prefix, vm.toString(externalJobId));
+  }
+
+  function getLastWebhookJobId(string memory nodeId) public returns(string memory) {
+    string[] memory cmds = new string[](4);
+    string memory prefix = "0_";
+    cmds[0] = "make";
+    cmds[1] = "fct-get-last-webhook-job-id";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("PREFIX=", prefix);
+    bytes memory jobId = vm.ffi(cmds);
+    return Utils.removePrefix(prefix, string(jobId));
+  }
+
+  function createDirectRequestJob(string memory nodeId, address oracle) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-direct-request-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("ORACLE_ADDRESS=", vm.toString(oracle));
+    vm.ffi(cmds);
+  }
+
+  function createCronJob(string memory nodeId, address cronConsumer) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-cron-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("CRON_CONSUMER_ADDRESS=", vm.toString(cronConsumer));
+    vm.ffi(cmds);
+  }
+
+  function createWebhookJob(string memory nodeId) public {
+    string[] memory cmds = new string[](3);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-webhook-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    vm.ffi(cmds);
+  }
+
+  function createKeeperJob(string memory nodeId, address registry) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-keeper-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("REGISTRY_ADDRESS=", vm.toString(registry));
+    vm.ffi(cmds);
+  }
+
+  function createFluxJob(string memory nodeId, address fluxAggregator) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-flux-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("FLUX_AGGREGATOR_ADDRESS=", vm.toString(fluxAggregator));
+    vm.ffi(cmds);
+  }
+
+  function createOcrBootstrapJob(string memory nodeId, address offchainAggregator) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-ocr-bootstrap-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("OFFCHAIN_AGGREGATOR_ADDRESS=", vm.toString(offchainAggregator));
+    vm.ffi(cmds);
+  }
+
+  function createOcrJob(string memory nodeId, address offchainAggregator, string memory bootstrapPeerId) public {
+    string[] memory cmds = new string[](5);
+    cmds[0] = "make";
+    cmds[1] = "fct-create-ocr-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("OFFCHAIN_AGGREGATOR_ADDRESS=", vm.toString(offchainAggregator));
+    cmds[4] = Utils.append("BOOTSTRAP_P2P_KEY=", bootstrapPeerId);
+    vm.ffi(cmds);
+  }
+
+  function runWebhookJob(string memory nodeId, string memory jobId) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-run-webhook-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[2] = Utils.append("WEBHOOK_JOB_ID=", jobId);
+    vm.ffi(cmds);
+  }
+
+  function deleteJob(string memory nodeId, string memory jobId) public {
+    string[] memory cmds = new string[](4);
+    cmds[0] = "make";
+    cmds[1] = "fct-delete-job";
+    cmds[2] = Utils.append("NODE_ID=", nodeId);
+    cmds[3] = Utils.append("JOB_ID=", jobId);
+    vm.ffi(cmds);
   }
 }

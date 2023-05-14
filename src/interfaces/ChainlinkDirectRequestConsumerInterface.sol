4fc27568be10336029d1499b97d9c43f2df62507
koteld <festbot@gmail.com>
2023-05-14 13:21:38 +0200

    supplement chainlink contracts interfaces

--- src/interfaces/ChainlinkDirectRequestConsumerInterface.sol
+++ src/interfaces/ChainlinkDirectRequestConsumerInterface.sol
@@ -3,5 +3,9 @@ pragma solidity >=0.6.2 <0.9.0;
 
 interface ChainlinkDirectRequestConsumerInterface {
   function requestEthereumPrice(address _oracle, string memory _jobId) external;
+  function fulfillEthereumPrice(bytes32 _requestId, uint256 _price) external;
+  function getChainlinkToken() external view returns (address);
+  function withdrawLink() external;
+  function cancelRequest(bytes32 _requestId, uint256 _payment, bytes4 _callbackFunctionId, uint256 _expiration) external;
   function currentPrice() external view returns (uint256);
 }

4e14ad2e0ee4dfdb7095edc36b984f9fda2dac0b
koteld <festbot@gmail.com>
2023-11-20 23:32:09 +0100

    add Automation and DataFeeds tests TODOs

---
+++ test/Automation.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2 <0.9.0;
+
+import "forge-std/Test.sol";
+
+import "./BaseTest.t.sol";
+import "script/automation/Automation.s.sol";
+import "src/interfaces/KeeperRegistrarInterface.sol";
+import "src/interfaces/KeeperRegistryInterface.sol";
+
+contract AutomationScriptTest is BaseTest {
+  // TODO: test automation script
+}
---
+++ test/DataFeeds.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2 <0.9.0;
+
+import "forge-std/Test.sol";
+
+import "./BaseTest.t.sol";
+import "script/feeds/DataFeed.s.sol";
+import "script/feeds/DataFeedProxy.s.sol";
+import "script/feeds/ENSFeedsResolver.s.sol";
+import "src/interfaces/AggregatorV2V3Interface.sol";
+import "src/interfaces/ENSResolverInterface.sol";
+
+contract DataFeedsScriptTest is BaseTest {
+  // TODO: test data feeds script
+}

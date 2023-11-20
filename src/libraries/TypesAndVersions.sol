6309b05f3b56534de0dff8428aab355255fc4a27
koteld <festbot@gmail.com>
2023-11-20 23:29:25 +0100

    add typesAndVersions contract library

---
+++ src/libraries/TypesAndVersions.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2 <0.9.0;
+
+library TypesAndVersions {
+  string public constant KeeperRegistrar1_1 = "KeeperRegistrar 1.1.0";
+  string public constant KeeperRegistrar2_0 = "KeeperRegistrar 2.0.0";
+}

e30bc5198bdfc35ac8c25b72b949751b34c6c0be
koteld <festbot@gmail.com>
2023-12-14 00:33:09 +0100

    introduce feeds script tests

--- test/DataFeeds.t.sol
+++ test/DataFeeds.t.sol
@@ -7,8 +7,77 @@ import "./BaseTest.t.sol";
 import "script/feeds/DataFeed.s.sol";
 import "script/feeds/ENSFeedsResolver.s.sol";
 import "src/interfaces/AggregatorV2V3Interface.sol";
-import "src/interfaces/ENSResolverInterface.sol";
+import "src/interfaces/MockV3AggregatorInterface.sol";
 
 contract DataFeedsScriptTest is BaseTest {
-  // TODO: test data feeds script
+  uint8 public DECIMALS = 18;
+  int256 public INITIAL_ANSWER = 1_000;
+  int256 public LATEST_ANSWER = 2_000;
+  uint80 public LATEST_ROUND = 1;
+  string public constant DESCRIPTION = "v0.6/tests/MockV3Aggregator.sol";
+  uint256 public VERSION = 0;
+
+  DataFeedsScript public dataFeedsScript;
+  address public linkTokenAddress;
+  address public dataFeedAddress;
+
+  function setUp() public override {
+    BaseTest.setUp();
+
+    vm.startBroadcast(OWNER_ADDRESS);
+    linkTokenAddress = deployCode("LinkToken.sol:LinkToken");
+    dataFeedAddress = deployCode("MockV3Aggregator.sol:MockV3Aggregator", abi.encode(DECIMALS, INITIAL_ANSWER));
+    dataFeedsScript = new DataFeedsScript(dataFeedAddress);
+    vm.stopBroadcast();
+  }
+
+  function test_GetLatestRound_Success() public {
+    MockV3AggregatorInterface dataFeed = MockV3AggregatorInterface(dataFeedAddress);
+    uint256 timestamp = block.timestamp;
+    vm.broadcast(OWNER_ADDRESS);
+    dataFeed.updateRoundData(LATEST_ROUND, LATEST_ANSWER, timestamp, timestamp);
+
+    (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    ) = dataFeedsScript.getLatestRoundData();
+    assertEq(roundId, LATEST_ROUND);
+    assertEq(answer, LATEST_ANSWER);
+    assertEq(startedAt, timestamp);
+    assertEq(updatedAt, timestamp);
+    assertEq(answeredInRound, LATEST_ROUND);
+  }
+
+  function test_GetRound_Success() public {
+    MockV3AggregatorInterface dataFeed = MockV3AggregatorInterface(dataFeedAddress);
+    uint256 timestamp = block.timestamp;
+    vm.broadcast(OWNER_ADDRESS);
+    dataFeed.updateRoundData(LATEST_ROUND, LATEST_ANSWER, timestamp, timestamp);
+
+    (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    ) = dataFeedsScript.getRoundData(LATEST_ROUND);
+    assertEq(roundId, LATEST_ROUND);
+    assertEq(answer, LATEST_ANSWER);
+    assertEq(startedAt, timestamp);
+    assertEq(updatedAt, timestamp);
+    assertEq(answeredInRound, LATEST_ROUND);
+  }
+
+  function test_GetDescription_Success() public {
+    string memory description = dataFeedsScript.getDescription();
+    assertEq(description, DESCRIPTION);
+  }
+
+  function test_GetVersion_Success() public {
+    uint256 version = dataFeedsScript.getVersion();
+    assertEq(version, VERSION);
+  }
 }

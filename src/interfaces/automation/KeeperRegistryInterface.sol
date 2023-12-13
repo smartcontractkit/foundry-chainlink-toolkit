d1cc54b035e36951fe1c816dfdc0a5eead014677
koteld <festbot@gmail.com>
2023-12-13 22:55:03 +0100

    update keeper registry interface

--- src/interfaces/automation/KeeperRegistryInterface.sol
+++ src/interfaces/automation/KeeperRegistryInterface.sol
@@ -7,18 +7,15 @@ import "../shared/TypeAndVersionInterface.sol";
 import "src/libraries/AutomationUtils.sol";
 
 interface KeeperRegistryInterface is TypeAndVersionInterface {
+  function getActiveUpkeepIDs(uint256 startIndex, uint256 maxCount) external view returns (uint256[] memory);
+  function addFunds(uint256 id, uint96 amount) external;
   function pauseUpkeep(uint256 id) external;
   function unpauseUpkeep(uint256 id) external;
   function cancelUpkeep(uint256 id) external;
-  function addFunds(uint256 id, uint96 amount) external;
   function withdrawFunds(uint256 id, address to) external;
-  function withdrawPayment(address from, address to) external;
   function setUpkeepGasLimit(uint256 id, uint32 gasLimit) external;
-  function upkeepTranscoderVersion() external view returns(AutomationUtils.UpkeepFormat);
-  function transferPayeeship(address keeper, address proposed) external;
-  function acceptPayeeship(address keeper) external;
   function transferUpkeepAdmin(uint256 id, address proposed) external;
   function acceptUpkeepAdmin(uint256 id) external;
-  function getActiveUpkeepIDs(uint256 startIndex, uint256 maxCount) external view returns (uint256[] memory);
   function getMinBalanceForUpkeep(uint256 id) external view returns (uint96 minBalance);
+  function upkeepTranscoderVersion() external view returns(AutomationUtils.UpkeepFormat);
 }

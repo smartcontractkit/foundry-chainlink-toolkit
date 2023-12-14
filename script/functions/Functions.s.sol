86197097cc6e8f22cf54744ced8f69afdaeab595
koteld <festbot@gmail.com>
2023-12-15 00:00:00 +0100

    update functions script: add constructor, update dependencies

--- script/functions/Functions.s.sol
+++ script/functions/Functions.s.sol
@@ -3,17 +3,19 @@ pragma solidity >=0.6.2 <0.9.0;
 
 import "forge-std/Script.sol";
 
-import "src/interfaces/IFunctionsRouter.sol";
-import "src/interfaces/IFunctionsBilling.sol";
-import "src/interfaces/IFunctionsSubscriptions.sol";
-import "src/interfaces/LinkTokenInterface.sol";
-import "src/interfaces/AccessControllerInterface.sol";
-import "src/libraries/Utils.sol";
 import "../helpers/BaseScript.s.sol";
+import "src/interfaces/functions/IFunctionsRouter.sol";
+import "src/interfaces/functions/IFunctionsBilling.sol";
+import "src/interfaces/functions/IFunctionsSubscriptions.sol";
+import "src/interfaces/shared/LinkTokenInterface.sol";
+import "src/interfaces/shared/AccessControllerInterface.sol";
+import "src/libraries/Utils.sol";
 
 contract FunctionsScript is BaseScript {
+  address public functionsRouterAddress;
+
   /// @notice MODIFIERS
-  modifier isAllowListed(address functionsRouterAddress) {
+  modifier isAllowListed() {
     IFunctionsRouter functionsRouter = IFunctionsRouter(functionsRouterAddress);
     bytes32 allowListId = functionsRouter.getAllowListId();
     if (allowListId == bytes32(0)) {
@@ -29,10 +31,13 @@ contract FunctionsScript is BaseScript {
     _;
   }
 
-  /// @notice WRAPPER FUNCTIONS
-  function createSubscription(
-    address functionsRouterAddress
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external returns(uint64) {
+  constructor (address _functionsRouterAddress) {
+    functionsRouterAddress = _functionsRouterAddress;
+  }
+
+  /// @notice Functions Router functions
+
+  function createSubscription() nestedScriptContext isAllowListed external returns(uint64) {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     uint64 subscriptionId = functionsRouter.createSubscription();
     console.log("Created subscription with ID:", subscriptionId);
@@ -40,9 +45,8 @@ contract FunctionsScript is BaseScript {
   }
 
   function createSubscriptionWithConsumer(
-    address functionsRouterAddress,
     address consumer
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external returns(uint64) {
+  ) nestedScriptContext isAllowListed external returns(uint64) {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     uint64 subscriptionId = functionsRouter.createSubscriptionWithConsumer(consumer);
     // Additional logic can be added here
@@ -51,17 +55,15 @@ contract FunctionsScript is BaseScript {
   }
 
   function cancelSubscription(
-    address functionsRouterAddress,
     uint64 subscriptionId,
     address receivingAddress
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     functionsRouter.cancelSubscription(subscriptionId, receivingAddress);
     console.log("Cancelled subscription with ID:", subscriptionId);
   }
 
   function getSubscription(
-    address functionsRouterAddress,
     uint64 subscriptionId
   ) external view returns(IFunctionsSubscriptions.Subscription memory) {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
@@ -69,61 +71,54 @@ contract FunctionsScript is BaseScript {
   }
 
   function proposeSubscriptionOwnerTransfer(
-    address functionsRouterAddress,
     uint64 subscriptionId,
     address newOwner
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     functionsRouter.proposeSubscriptionOwnerTransfer(subscriptionId, newOwner);
     console.log("Proposed subscription owner transfer for ID:", subscriptionId);
   }
 
   function acceptSubscriptionOwnerTransfer(
-    address functionsRouterAddress,
     uint64 subscriptionId
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     functionsRouter.acceptSubscriptionOwnerTransfer(subscriptionId);
     console.log("Accepted subscription owner transfer for ID:", subscriptionId);
   }
 
   function addConsumer(
-    address functionsRouterAddress,
     uint64 subscriptionId,
     address consumer
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     functionsRouter.addConsumer(subscriptionId, consumer);
     console.log("Added consumer to subscription ID:", subscriptionId);
   }
 
   function removeConsumer(
-    address functionsRouterAddress,
     uint64 subscriptionId,
     address consumer
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     functionsRouter.removeConsumer(subscriptionId, consumer);
     console.log("Removed consumer from subscription ID:", subscriptionId);
   }
 
   function timeoutRequests(
-    address functionsRouterAddress,
     FunctionsResponse.Commitment[] memory commitments
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     require(commitments.length > 0, "Must provide at least one request commitment");
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     functionsRouter.timeoutRequests(commitments);
     console.log("Timed out requests");
   }
 
-  /// @notice SYNTHETIC FUNCTIONS
   function fundSubscription(
-    address functionsRouterAddress,
     address linkTokenAddress,
     uint256 juelsAmount,
     uint64 subscriptionId
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external {
+  ) nestedScriptContext isAllowListed external {
     IFunctionsSubscriptions functionsRouter = IFunctionsSubscriptions(functionsRouterAddress);
     LinkTokenInterface linkToken = LinkTokenInterface(linkTokenAddress);
 
@@ -142,12 +137,11 @@ contract FunctionsScript is BaseScript {
   }
 
   function estimateRequestCost(
-    address functionsRouterAddress,
     string memory donId,
     uint64 subscriptionId,
     uint32 callbackGasLimit,
     uint256 gasPriceWei
-  ) nestedScriptContext isAllowListed(functionsRouterAddress) external returns(uint96) {
+  ) nestedScriptContext isAllowListed external returns(uint96) {
     require(gasPriceWei > 0, "Gas price must be greater than 0");
     require(callbackGasLimit > 0, "Callback gas limit must be greater than 0");
 

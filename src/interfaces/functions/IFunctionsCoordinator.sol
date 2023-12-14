6b0521a7596b63c2a62b892628acc9f6f7ab1d65
koteld <festbot@gmail.com>
2023-12-14 23:56:40 +0100

    reorder interfaces in the repo

--- src/interfaces/functions/IFunctionsCoordinator.sol
+++ src/interfaces/functions/IFunctionsCoordinator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
 
-import {FunctionsResponse} from "../libraries/FunctionsResponse.sol";
+import { FunctionsResponse } from "src/libraries/FunctionsResponse.sol";
 
 /// @title Chainlink Functions DON Coordinator interface.
 interface IFunctionsCoordinator {
